6ed72d29692680d5f7292d17650026ff18a45b21
Joshua Beckom <jbeckom@gmail.com>
2018-01-10 10:31:08 -0500

    adding SSDT proj

---
+++ AdventureWorksDW2012/AdventureWorksDW2012/dbo/Views/Total_DimTerritoryAndGeography.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE VIEW dbo.Total_DimTerritoryAndGeography
+AS
+SELECT	g.City
+		,g.StateProvinceCode
+		,g.StateProvinceName
+		,g.CountryRegionCode
+		,g.EnglishCountryRegionName
+		,t.SalesTerritoryGroup
+		,t.SalesTerritoryRegion
+FROM	dbo.DimGeography	AS g
+	INNER JOIN	dbo.DimSalesTerritory AS t
+		ON	g.SalesTerritoryKey = t.SalesTerritoryKey
---
+++ AdventureWorksDW2012/AdventureWorksDW2012/dbo/Views/Total_FactInternetSales.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE VIEW	dbo.Total_FactInternetSales
+WITH SCHEMABINDING
+AS
+	SELECT	SUM(fis.DiscountAmount)			AS [Total_DiscountAmount]
+			,SUM(fis.ProductStandardCost)	AS [Total_ProductStandardCost]
+			,SUM(fis.TotalProductCost)		AS [Total_TotalProductCost]
+			,SUM(fis.SalesAmount)			AS [Total_SalesAmount]
+			,fis.OrderDate
+			,fis.CustomerKey
+			,fis.CurrencyKey
+			,COUNT_BIG(*)					AS [RecordCount]
+
+	FROM	dbo.FactInternetSales	AS fis
+
+	GROUP BY	fis.OrderDate, fis.CustomerKey, fis.CurrencyKey;
+
+GO
+CREATE UNIQUE CLUSTERED INDEX [IX_Total_FactInternetSales]
+    ON [dbo].[Total_FactInternetSales]([OrderDate] ASC, [CustomerKey] ASC, [CurrencyKey] ASC);
+
